ba8e5d5eb2bf9c2f334c7c55f76e514efd3c8f3d
Jeralan <jeremyaangel@gmail.com>
2023-03-02 00:00:02 -0600

    Convo summarization to avoid token limits

--- convoGPT.py
+++ convoGPT.py
@@ -4,6 +4,39 @@ import sys, getopt, os
 helpString = '''/exit - exit and save conversation
 /c <text> - replace last bot response with text'''
 
+def getResponse(convo: str, username: str) -> tuple[str,str]:
+    model_engine = "text-davinci-003"
+    temperature = 0.5
+    maxTokens = 250
+    response = None
+    savedLines = ""
+    try:
+        response = openai.Completion.create(engine=model_engine,
+                                                    prompt=convo,
+                                                    temperature=temperature,
+                                                    max_tokens=maxTokens)
+        response = response.choices[0].text.strip()
+        response = response.split(username+":")[0].strip()
+    except openai.error.InvalidRequestError:
+        splitConvo = convo.split("\n")
+        firstHalfConvo = "\n".join(splitConvo[:len(splitConvo)//2])
+        secondHalfConvo = "\n".join(splitConvo[len(splitConvo)//2:])
+        summaryPrompt = firstHalfConvo+"\n\nSo to describe this conversation in summary:\n"
+        summary = openai.Completion.create(engine=model_engine,
+                                                    prompt=summaryPrompt,
+                                                    temperature=temperature,
+                                                    max_tokens=maxTokens)
+        summary = summary.choices[0].text.strip()
+        convo = "So a summary of the conversation so far is:\n"+summary+"\n\nThe conversation then continued as follows:\n\n"+secondHalfConvo
+        response = openai.Completion.create(engine=model_engine,
+                                                    prompt=convo,
+                                                    temperature=temperature,
+                                                    max_tokens=maxTokens)
+        response = response.choices[0].text.strip()
+        response = response.split(username+":")[0].strip()
+    convo += response+f"\n{username}: "
+    return convo,response
+
 def main(argv: list[str]):
     file = "openAIkey.txt"
     opts, args = getopt.getopt(argv,"hk:",["key="])
@@ -29,9 +62,6 @@ def main(argv: list[str]):
     print(convo.split("\n")[-2])
     print("(/h to view all available commands)")
     userIn = input(convo.split("\n")[-1])
-    model_engine = "text-davinci-003"
-    temperature = 0.5
-    maxTokens = 250
     while not ("/exit" in userIn):
         if userIn[:2] == "/c":
             splitConvo = convo.split("\n")
@@ -41,13 +71,7 @@ def main(argv: list[str]):
             print(helpString)
         else:
             convo += userIn+f"\n{botname}: "
-            response = openai.Completion.create(engine=model_engine,
-                                                prompt=convo,
-                                                temperature=temperature,
-                                                max_tokens=maxTokens)
-            response = response.choices[0].text.strip()
-            response = response.split(username+":")[0].strip()
-            convo += response+f"\n{username}: "
+            convo,response = getResponse(convo,username)
             print(f"{botname}: {response}")
         userIn = input(convo.split("\n")[-1])
     f = open(filename, "w")
